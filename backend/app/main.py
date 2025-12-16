b638bb3cfd968c7fe8ceddc7edbd58f89ffa1923
manfoya <manfoya83@gmail.com>
2025-12-16 17:35:02 +0100

    feat(core): register settings router in main application

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@
 from fastapi import FastAPI
 from app.api.v1 import auth
 from app.models import users, zones, survey, settings
-from app.api.v1 import auth, users, maps
+from app.api.v1 import auth, users, maps, settings
 
 
 app = FastAPI(
@@ -15,7 +15,8 @@ app = FastAPI(
 # On inclut nos routes
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Authentification"])
 app.include_router(users.router, prefix="/api/v1/users", tags=["Utilisateurs"])
-app.include_router(maps.router, prefix="/api/v1/maps", tags=["Maps & Quotas"]) 
+app.include_router(maps.router, prefix="/api/v1/maps", tags=["Maps & Quotas"])
+app.include_router(settings.router, prefix="/api/v1/settings", tags=["Global Settings"]) 
 
 
 @app.get("/")
