bcc5ac829198704ce24eaace71305932184fade5
manfoya <manfoya83@gmail.com>
2026-01-30 04:21:15 +0100

    config(app): update CORS settings and app description

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,8 @@
 # backend/app/main.py
 
 from fastapi import FastAPI
-from app.api.v1 import auth
-from app.api.v1 import auth, users, maps, settings, variables, quotas
-from fastapi.middleware.cors import CORSMiddleware # <--- L'IMPORT CRUCIAL
-
-# Nettoyage des imports (tu avais des doublons)
-from app.api.v1 import auth, users, maps, settings, dictionary
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.v1 import auth, users, maps, settings, variables, quotas, dictionary, stats, messages
 
 app = FastAPI(
     title="Open Survey Monitor API",
@@ -18,22 +14,28 @@ app = FastAPI(
     ],
 )
 
-# En production, on mettra l'URL réelle du site au lieu de "*"
+# configuration cors
 origins = ["*"]
 
-# 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# On inclut nos routes
+# inclusion des routeurs
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Authentification"])
 app.include_router(users.router, prefix="/api/v1/users", tags=["Utilisateurs"])
 app.include_router(maps.router, prefix="/api/v1/maps", tags=["Maps & Quotas"])
 app.include_router(settings.router, prefix="/api/v1/settings", tags=["Global Settings"])
 app.include_router(variables.router, prefix="/api/v1/variables", tags=["Variables"]) 
 app.include_router(quotas.router, prefix="/api/v1/quotas", tags=["Quotas"]) 
-# Ajout du module de stats
-from app.api.v1 import stats
+app.include_router(dictionary.router, prefix="/api/v1/dictionary", tags=["Dictionnaire"])
 app.include_router(stats.router, prefix="/api/v1/stats", tags=["Statistiques"])
+app.include_router(messages.router, prefix="/api/v1/messages", tags=["Messagerie"])
 
 @app.get("/")
 def read_root():
-    return {"message": "Bienvenue sur l'API Open Survey Monitor"}
+    return {"message": "bienvenue sur l'api open survey monitor"}
